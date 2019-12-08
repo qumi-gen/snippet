04136f0a96719dfa6c1297a68eaf611804da19d3
qumi-gen <35165086+qumi-gen@users.noreply.github.com>
2019-12-08 18:18:21 +0900

    Update tutorial.js

--- src/react/tutorial.js
+++ src/react/tutorial.js
@@ -1,16 +1,10 @@
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
   render() {
     return (
       <button className="square" 
-        onClick={()=> this.setState({value: 'X'})}
+        onClick={()=> this.props.onClick()}
         >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -24,7 +18,11 @@ class Board extends React.Component {
     };
   }
   renderSquare(i) {
-    return <Square value={this.state.squares[i]}/>;
+    return 
+    <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />;
   }
 
   render() {
